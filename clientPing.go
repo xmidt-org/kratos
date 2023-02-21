c135e2c72a455ede205a30b5044864e6fbc6b865
Marcia Piccione <mpicci200@Marcias-MBP.hsd1.pa.comcast.net>
2023-02-21 08:02:55 -0500

    remove debug prints

--- clientPing.go
+++ clientPing.go
@@ -1,7 +1,6 @@
 package kratos
 
 import (
-	"fmt"
 	"time"
 
 	"go.uber.org/zap"
@@ -16,27 +15,22 @@ type HandlePingMiss func() error
 // given interval.  If a ping is missed, we call the client's HandlePingMiss
 // function.
 func (c *client) checkPing(inTimer *time.Timer, pinged <-chan string) {
-	fmt.Println("in checkPing")
 	defer c.wg.Done()
 	// pingMiss indicates that a ping has been missed.
 	pingMiss := false
 	c.logger.Info("Watching socket for pings")
 	count := 0
-	fmt.Println("about to go into for")
 	// as long as we're getting pings, we continue to loop.
 	for !pingMiss {
-		fmt.Println("in select")
 		select {
 
 		// if we get a done signal, we leave the function.
 		case <-c.done:
 			c.logger.Info("Stopped waiting for pings")
-			fmt.Println("done")
 			return
 			// if we get a ping, make sure to reset the timer until the next ping.
 		case <-pinged:
 			count = 0
-			fmt.Println("pinged")
 			if !inTimer.Stop() {
 				<-inTimer.C
 			}
@@ -46,20 +40,17 @@ func (c *client) checkPing(inTimer *time.Timer, pinged <-chan string) {
 
 		// if we hit the timer, we've missed a ping.
 		case <-inTimer.C:
-			fmt.Println("inTimer")
 			c.logger.Error("Ping miss, calling handler", zap.Int("count", count))
 			err := c.handlePingMiss()
 			if err != nil {
 				c.logger.Error("Error handling ping miss:", zap.Error(err))
 			}
 			if count >= c.pingConfig.MaxPingMiss {
-				fmt.Println("exiting ping loop")
 				c.logger.Error("Ping miss, exiting ping loop")
 				pingMiss = true
 			}
 			c.logger.Debug("Resetting ping timer")
 			inTimer.Reset(c.pingConfig.PingWait)
 		}
-		fmt.Println("end of checkPing")
 	}
 }
